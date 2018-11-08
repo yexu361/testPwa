e120a12acff926a7ba41c4478be5beb86cd10ba3
dongyifei <dongyifei@epaynfc.com>
2018-11-08 12:59:58 +0800

    1

--- sw.js
+++ sw.js
@@ -7,11 +7,10 @@ var cacheList=[
     '155.png'
 ]
 
-/*
 self.addEventListener('install',e =>{
     e.waitUntil(
         caches.open(cacheStorageKey)
             .then(cache => cache.addAll(cacheList))
             .then(() => self.skipWaiting())
     )
-})*/
+})
