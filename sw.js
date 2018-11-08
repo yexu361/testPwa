6a106ff04318613773a05c2849039bd097ef1ce7
dongyifei <dongyifei@epaynfc.com>
2018-11-08 12:19:10 +0800

    1

---
+++ sw.js
@@ -0,0 +1,17 @@
+importScripts("workbox-sw.js");
+var cacheStorageKey = 'minimal-pwa-1'
+var cacheList=[
+    '/',
+    'index.html',
+    'main.css',
+    '155.png'
+]
+
+/*
+self.addEventListener('install',e =>{
+    e.waitUntil(
+        caches.open(cacheStorageKey)
+            .then(cache => cache.addAll(cacheList))
+            .then(() => self.skipWaiting())
+    )
+})*/
